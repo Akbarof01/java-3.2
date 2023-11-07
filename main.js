c0f7fe0464f0550d1745fe15442977718c85d971
Akbarof01 <akhbbarov01@gmail.com>
2023-11-07 20:44:11 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,391 @@
+//03.11.23 darsdiki
+// function a(){
+//     let a=+prompt("a ni kiriting")
+//     let b=+prompt("b ni kiriting")
+//     console.log(a+b);
+// }
+// a();
+// function a(){
+//     let a=+prompt("sonni kiriting")
+//     console.log(a*60);
+// }
+// a();
+// function a(){
+//     let a=+prompt("sonni kiriting")
+//     console.log(a+1);
+// }
+// a();
+// 4.Funskiya uchburchakning asosi va balandligini qabul qiladi.
+// Funksiya uchburchakning yuzini hisoblab qaytarsin. Uchburchak yuzini topish formulasi: S = (asos * balandlik) / 2
+// function a(){
+//     let a=+prompt("asosni kiriting")
+//     let h=+prompt("balandlikni kiriting")
+//     console.log("S = " +a*h);
+// }
+// a();
+// 5.Funksiya yoshni yil ko’rinishida qabul qiladi. ushbu funksiya
+// yoshni kunlarda hisoblab natijani qaytarsin. Bir yil 365 kun deb
+// oling
+// function a(){
+//     let yosh=+prompt("yoshni kiriting")
+//     console.log(yosh*365);
+// }
+// a();
+// 6.Quyidagi namunalarga muvofiq ravishda funksiyani tana
+// qismini yozing.
+// kub(3) ᔍ 27
+// kub(5) ᔍ 125
+// kub(10) ᔍ 1000
+// function a(){
+//     let son=+prompt("sonni kiriting")
+//     console.log(son**3);
+// }
+// a();
+// 8.Funksiya 2ta argument qabul qilsin. Ushbu funksiya birinchi
+// qiymatning ikkinchi qiymat bilan ko’paytmasini qaytarsin.
+// function a(){
+//     let a=+prompt("a sonni kiriting")
+// let b=+prompt("b sonni kiriting")
+// console.log(a*b);
+// }
+// a();
+// 9.Soat ko’rinishida qiymat kiritilsa, sekundlarda qaytaradigan
+// funksiya yasang
+// function a(){
+//     let a=+prompt("sonni kiriting")
+//     console.log(a*3600);
+// }
+// a();
+// 10.Uchburchakning uchinchi tomonining eng uzun qiymatini
+// qaytaradigan funksiya yasang.
+// function a(){
+//     let a=+prompt("a ni kiriting")
+//     let b=+prompt("b ni kiriting")
+//     let c=+prompt("c ni kiriting")
+//     if (a>b) {
+//         if (a>c){
+//         console.log(a);   
+//         }else {console.log(c);}
+//     }else if (b>c){
+//         console.log(b);
+//     } 
+//     else{
+//         console.log(c);
+//     }
+// }
+// a();
+// 11.Ikkita son argument sifatida kiritilsa, ushbu ikkala sonni bir inchisini ikkinchisiga bo’lgandagi qoldiqni qaytarib beradigan
+// funksiya yasang
+// function a(){  
+//    let a=+prompt("a ni kiriting")
+//     let b=+prompt("b ni kiriting")
+//     console.log(a%b);
+// }
+// a();
+// 12.Turtburchakning bo’yi va eni berilsa uning yuzini hisoblab
+// qaytaradigan funksiya yasang. Formula S = bo’yi * eni
+// function a(){
+//     let a=+prompt("a ni kiriting")
+//     let b=+prompt("b ni kiriting")
+//     console.log("S= "+a*b);
+// }
+// a();
+
+
+//function  toSekunds(minutes){
+   // console.log(minutes*60);
+//}
+//toSekunds(5);
+//toSekunds(4);                                                                       
+//toSekunds(3);
+//toSekunds(2);
+
+//function  nextInteger(int){
+   // console.log(int+1);
+//}
+//nextInteger(0)
+//nextInteger(9)
+//nextInteger(-3)
+
+//function  uchburchakYuzi(asos, balandlik){
+//    console.log(0.5 *asos* balandlik);
+//}
+//uchburchakYuzi(3, 2)
+//uchburchakYuzi(7, 4)
+//uchburchakYuzi(10, 10)
+
+
+//function  ageToDays(age){
+    //    console.log(age*365);
+   // }
+  //  ageToDays(65)
+  //  ageToDays(0)
+  //  ageToDays(20)//
+
+
+  //function kub(a){
+   // console.log(a**3);
+
+  //}
+ // kub(3)
+ // kub(5)
+ // kub(10)
+
+
+  //function power(x,y) {
+   // console.log(x * y)
+   // }
+   // power(230, 10)
+   // power(110, 3)
+   // power(480, 20)
+
+
+   //function hourToSekunds(hour) {
+    //console.log(hour * 3600)
+   // }
+   // hourToSekunds(2)
+   // hourToSekunds(10)
+    //hourToSekunds(24)
+   // let a = 2
+   // let b = 5
+
+    //let c = prompt("Son");  
+    //if (b > a) {
+    //    console.log(5)
+    //}
+   // else{
+    //    console.log(2)
+///}
+       // function JuftmiToq (){
+       // let son = +prompt("son");
+
+       // if (son % 2 == 0 ) {
+           //console.log("Juft")
+        //} else{
+         //   console.log("Toq")
+       // }
+   // }
+      //  JuftmiToq();
+      //function firstElement(arr) {
+       // console.log(arr)
+       // }
+       //function power(x,y) {
+      //  console.log(x * y)
+      //  }
+      //  power(230, 10)
+       // power(110, 3) 
+       // power(480, 20)
+      // function uchinchiTomon(tomon1, tomon2) {
+      //   console.log("10. uchinchiTomon(): " + (tomon1 + tomon2 - 1));
+      // }
+      // uchinchiTomon(8 , 10)
+      // uchinchiTomon(5 , 7)
+      // uchinchiTomon(9 , 2) 
+
+
+
+
+
+
+      //06.11.23
+//1 function a(){
+//     let a=+prompt("a ni kiriting")
+//     let b=+prompt("b ni kiriting")
+//     console.log(a+b);
+// }
+// a();
+
+//2 function a(){
+//     let a=+prompt("sonni kiriting")
+//     console.log(a*60);
+// }
+// a();
+// 3
+// function a(){
+//     let a=+prompt("sonni kiriting")
+//     console.log(a+1);
+// }
+// a();
+// 4
+// function a(){
+//     let a=+prompt("asosni kiriting")
+//     let h=+prompt("balandlikni kiriting")
+//     console.log("S = " +a*h);
+// }
+// a();
+// 5
+// function a(){
+//     let yosh=+prompt("yoshni kiriting")
+//     console.log(yosh*365);
+// }
+// a();
+// 6
+// function a(){
+//     let son=+prompt("sonni kiriting")
+//     console.log(son**3);
+// }
+// a();
+// 8
+// function a(){
+//     let a=+prompt("a sonni kiriting")
+// let b=+prompt("b sonni kiriting")
+// console.log(a*b);
+// }
+// a();
+// 9
+// function a(){
+//     let a=+prompt("sonni kiriting")
+//     console.log(a*3600);
+// }
+// a();
+// 10
+// function a(){
+//     let a=+prompt("a ni kiriting")
+//     let b=+prompt("b ni kiriting")
+//     let c=+prompt("c ni kiriting")
+//     if (a>b) {
+//         if (a>c){
+//         console.log(a);   
+//         }else {console.log(c);}
+//     }else if (b>c){
+//         console.log(b);
+//     } else{
+//         console.log(c);
+//     }
+// }
+// a();
+// 11
+// function a(){  
+//    let a=+prompt("a ni kiriting")
+//     let b=+prompt("b ni kiriting")
+//     console.log(a%b);
+// }
+// a();
+// 12
+// function a(){
+//     let a=+prompt("a ni kiriting")
+//     let b=+prompt("b ni kiriting")
+//     console.log("S= "+a*b);
+// }
+// a();
+// 13
+// function a(){
+//     let a="Something "+prompt("so'zni kiriting")
+// console.log(a);
+// }
+// a();
+// 14
+// function a(){
+//     let a=+prompt("sonni kiriting")
+//     console.log(a**2);
+// }
+// a();
+// 15
+// function a(){
+//     let a=+prompt("sonni kiriting")
+//     if (a<=0){
+//         console.log("rost");
+//     }else{
+//         console.log("yolg'on");
+//     }
+// }
+// a();
+// 16
+// function a(){
+//     let n=+prompt("sonni kiriting")
+//     console.log((n-2)*180);
+// }
+// a();
+// 17
+// function a(){
+//     let a=+prompt("a ni kiriting")
+//     let b=+prompt("b ni kiriting")
+//     console.log(a*2+b*3);
+// }
+// a();
+// 18
+// function a(){
+//     let a=prompt("so'zni kriting")+" Edabit"
+//     console.log(a);
+// }
+// a();
+// 19
+// function a(){
+//     let a=+prompt("a sonni kiriting")
+//     let b=+prompt("b sonni kiriting")
+//     if (a+b<100){
+//         console.log("rost");
+//     }else{
+//         console.log("yolg'on");
+//     }
+// }
+// a();
+// 21
+// function a(){
+//     let a=+prompt("tovuqlar soni")
+//     let b=+prompt("qo'ylar soni")
+//     let c=+prompt("sigirlar soni")
+//     console.log(a*2+b*4+c*4);
+// }
+// a();
+// 22
+// function a(a,b){
+// return a&&b;
+// }
+// console.log(a(true,false));
+// console.log(a(true,true));
+// console.log(a(false,true));
+// console.log(a(false,false));
+// 23
+// function a(){
+//     let a=+prompt("a ni kiriting")
+//     let b=+prompt("b ni kiriting")
+//     if (a==b){
+//         console.log("rost");
+//     }else{
+//         console.log("yolg'on");
+//     }
+// }
+//   a();
+// 24
+// function a(){
+//     let a=+prompt("yutishlar soni")
+//     let b=+prompt("duranglar soni")
+//     let c=+prompt("mag'lubiyat soni")
+//     console.log(a*3+b*1+c*0);
+// }
+// a();
+// 25
+// function a(){
+//     let a=+prompt("soatni kiriting")
+//     let b=+prompt("minutni kiriting")
+//     console.log(a*3600+b*60+" sekund");
+// }
+// a();
+// 26
+// function a(){
+//     let n=prompt("sonni kiriting")
+
+// }
+// a();
+// 27
+// function a(){
+//     let a=+prompt("1-qiymatni kiriting")
+// let b=+prompt("2-qiymatni kiriting")
+//     console.log(typeof(a)==typeof(b));
+// }
+// a();
+// 28
+// function a(){
+//     let n=prompt("qiymatni kiriting")
+// console.log(n);
+// }
+// a();
+// 30
+// function a(){
+//     let a=+prompt("minutni kiriting")
+//     let b=+prompt("sekundni kiriting")
+//     console.log((a*60)*(b*1));
+// }
+// a();
+         
